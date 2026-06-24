50875a590eaa9a0a8cf580beb4ea05039397c1f4
Sébastien Buchoux <sebastien.buchoux@gmail.com>
2026-06-24 13:41:45 +0200

    chore: make fontawesome working without internet

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
 	import { Presentation } from '@seb-buch/slyd';
 
 	import '@seb-buch/slyd/themes/default.css';
+	import '$lib/assets/fontawesome/css/all.min.css';
 
 	import {
 		AsymmetricCryptoSlide,
@@ -33,12 +34,16 @@
 	import '$lib/assets/additional.css';
 
 	import PasskeyTypes from '$lib/conclusion/PasskeyTypes.svelte';
-	import RegistrationOptionsCreationSlide from '$lib/webauthn/RegistrationOptionsCreationSlide.svelte';
-	import RegistrationPasskeyCreationSlide from '$lib/webauthn/RegistrationPasskeyCreationSlide.svelte';
+	import RegistrationOptionsCreationSlide
+		from '$lib/webauthn/RegistrationOptionsCreationSlide.svelte';
+	import RegistrationPasskeyCreationSlide
+		from '$lib/webauthn/RegistrationPasskeyCreationSlide.svelte';
 	import RegistrationVerificationSlide from '$lib/webauthn/RegistrationVerificationSlide.svelte';
-	import AuthenticationOptionsCreationSlide from '$lib/webauthn/AuthenticationOptionsCreationSlide.svelte';
+	import AuthenticationOptionsCreationSlide
+		from '$lib/webauthn/AuthenticationOptionsCreationSlide.svelte';
 	import AuthenticationPasskeyGetSlide from '$lib/webauthn/AuthenticationPasskeyGetSlide.svelte';
-	import AuthenticationVerificationSlide from '$lib/webauthn/AuthenticationVerificationSlide.svelte';
+	import AuthenticationVerificationSlide
+		from '$lib/webauthn/AuthenticationVerificationSlide.svelte';
 	import PlanktonPhishingSlide from '$lib/story/PlanktonPhishingSlide.svelte';
 	import PlanktonVictorySlide from '$lib/story/PlanktonVictorySlide.svelte';
 	import PhishingEmailMFASlide from '$lib/story/PhishingEmailMFASlide.svelte';
@@ -47,7 +52,6 @@
 
 <svelte:head>
 	<title>Passkeys &ndash; Sébastien Buchoux &ndash; BreizhCamp 2026</title>
-	<script src="https://kit.fontawesome.com/b8d279694e.js" crossorigin="anonymous"></script>
 </svelte:head>
 
 <Presentation>
@@ -64,7 +68,7 @@
 		<p>
 			Comme tous les matins, Bob doit se connecter sur krabsvault.<br />
 			C'est l'application développée par Mr Krabs pour sécuriser la recette des patés de crabe.<br
-			/>
+		/>
 			Car, oui, même si Bob passe ses journées à cuisiner des patés de crabe,<br />
 			il se connecte à krabsvault pour relire la recette.<br />
 			Un peu comme les pilotes d'avion avec leur checklist avant chaque vol.<br />
